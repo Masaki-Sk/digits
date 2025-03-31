6fe8e10a171d84727567b424f057935e14a7a963
Masaki-Sk <skmmks320@gmail.com>
2025-03-30 22:10:15 -1000

    Round 2

---
+++ src/components/ContactCard.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { Card, Image } from 'react-bootstrap';
+import { Contact } from '@/lib/validationSchemas';
+
+/* Renders a single row in the List Stuff table. See list/page.tsx. */
+const StuffItem = ({ contact }: { contact: Contact }) => (
+  <Card>
+    <Card.Header>
+      <Image src={contact.image} alt={contact.firstName} height={100} width={100} />
+      <Card.Title>
+        {contact.firstName}
+        &nbsp;
+        {contact.lastName}
+      </Card.Title>
+      <Card.Subtitle>{contact.address}</Card.Subtitle>
+    </Card.Header>
+    <Card.Body>
+      <Card.Text>{contact.description}</Card.Text>
+    </Card.Body>
+  </Card>
+);
+
+export default StuffItem;
