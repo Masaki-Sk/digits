984b04c940fb5b87acd03f2d89ebf16906d4a928
Masaki-Sk <skmmks320@gmail.com>
2025-04-05 22:31:19 -1000

    Round 1

--- src/components/ContactCardAdmin.tsx
+++ src/components/ContactCardAdmin.tsx
@@ -1,10 +1,11 @@
 'use client';
 
-import { Card, Image } from 'react-bootstrap';
-import { Contact } from '@prisma/client';
+import { Card, Image, ListGroup } from 'react-bootstrap';
+import { Contact, Note } from '@prisma/client';
+import NoteItem from '@/components/NoteItem';
 
 /* Renders a single row in the List Stuff table. See list/page.tsx. */
-const StuffItem = ({ contact }: { contact: Contact }) => (
+const ContactCardAdmin = ({ contact, notes }: { contact: Contact, notes: Note[] }) => (
   <Card>
     <Card.Header>
       <Image src={contact.image} alt={contact.firstName} height={100} width={100} />
@@ -17,9 +18,12 @@ const StuffItem = ({ contact }: { contact: Contact }) => (
     </Card.Header>
     <Card.Body>
       <Card.Text>{contact.description}</Card.Text>
+      <ListGroup variant="flush">
+        {notes.map((note) => <NoteItem key={note.id} note={note} />)}
+      </ListGroup>
     </Card.Body>
     <Card.Footer className="blockquote-footer">{contact.owner}</Card.Footer>
   </Card>
 );
 
-export default StuffItem;
+export default ContactCardAdmin;
